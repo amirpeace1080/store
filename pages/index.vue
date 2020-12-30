6961a174f0dd9b69f1f3b8b0d707f525478d216d
amirpeace1080 <amirpeace1080@gmail.com>
2020-12-30 09:11:39 +0330

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home-page">
+    <Navbar />
+    <h1 class="title has-text-centered mt-4">Home Page</h1>
+  </div>
+</template>
+
+<script>
+import Navbar from '@/components/Navbar.vue'
+
+export default {
+  components:{
+    Navbar
+  }
+}
+</script>
+
+<style>
+
+</style>
